4cb7ddc4a4306d1fc324d9b6e9c4ffc9de26accd
iganao <naokiiga0504@gmail.com>
2021-08-24 22:20:04 +0900

    投稿のview

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,14 @@
 class Article < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :country
+    validates :season
+    validates :city
+    validates :English
+    validates :nice
+    validates :cleanliness
+    validates :content
+  end
+
 end
