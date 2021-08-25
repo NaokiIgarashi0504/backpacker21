14168e288f7bd69c0bb1396f83d1276e7b84e7f1
iganao <naokiiga0504@gmail.com>
2021-08-25 15:19:39 +0900

    ストロングパラメーター　投稿完了view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,13 @@ class ArticlesController < ApplicationController
     @articles = Article.new
   end
 
+  def create
+    Article.create(article_params)
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:image, :country, :city, :season, :english_id, :safe_id, :nice_id, :content)
+  end
+
 end
