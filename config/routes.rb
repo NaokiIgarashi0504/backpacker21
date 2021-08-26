62ca3afdb7be6849c7204df351bb17db62d78697
iganao <naokiiga0504@gmail.com>
2021-08-26 20:45:50 +0900

    コメント機能　表示

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'articles#index'
   resources :articles
+
+  resources :articles do
+    resources :comments, only: :create
+  end
 end
