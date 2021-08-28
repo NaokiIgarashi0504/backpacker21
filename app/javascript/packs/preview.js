cb7171b381d0f816254eb49e29975c0aecff64f3
iganao <naokiiga0504@gmail.com>
2021-08-28 11:10:04 +0900

    新規投稿画面　複数写真のプレビュー表示　データベースに保存

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,23 +1,40 @@
 document.addEventListener('DOMContentLoaded', function(){
-  if ( document.getElementById('prototype_title')){
+  if ( document.getElementById('prototype_image')){
     const ImageList = document.getElementById('image-list');
 
     const createImageHTML = (blob) => {
+
       const imageElement = document.createElement('div');
+      imageElement.setAttribute('class', "image-element")
+      let imageElementNum = document.querySelectorAll('.image-element').length
+
       const blobImage = document.createElement('img');
       blobImage.setAttribute('src', blob);
-      imageElement.appendChild(blobImage);
-      ImageList.appendChild(imageElement);
-    };
+
+      const inputHTML = document.createElement('input')
+      inputHTML.setAttribute('id', `prototype_image_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'article[images][]')
+      inputHTML.setAttribute('type', 'file')
+
+      imageElement.appendChild(blobImage)
+      imageElement.appendChild(inputHTML)
+      ImageList.appendChild(imageElement)
+
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+    
+        createImageHTML(blob)
+      })
+    }
 
 
-    document.getElementById('prototype_title').addEventListener('change', function(e){
-      const imageContent = document.querySelector('img');
-      
+    document.getElementById('prototype_image').addEventListener('change', function(e){
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
       
       createImageHTML(blob);
-    });
-  }
-});
+      
+      });
+    }
+  });
