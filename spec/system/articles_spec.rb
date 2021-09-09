bae608510e045b689d0def7ccec133b93d34c095
iganao <naokiiga0504@gmail.com>
2021-09-09 11:48:37 +0900

    id変更　JavaScriptのプレビューのimagesも変更済み

--- spec/system/articles_spec.rb
+++ spec/system/articles_spec.rb
@@ -2,8 +2,39 @@ require 'rails_helper'
 
 RSpec.describe "Articles", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
+    @article = FactoryBot.create(:article)
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
+  context 'ツイート投稿ができるとき'do
+    it 'ログインしたユーザーは新規投稿できる' do
+      # ログインする
+      visit new_user_session_path
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq(root_path)
+      # 新規投稿ページへのボタンがあることを確認する
+      expect(page).to have_content('New Article')
+      # 投稿ページに移動する
+      visit new_article_path
+      # フォームに情報を入力する
+      fill_in 'prototype_image', with: @tweet_image
+      fill_in 'prototype_catch_copy', with: @tweet_text
+      fill_in 'prototype_concept', with: @tweet_image
+      fill_in 'prototype_image', with: @tweet_text
+      # 送信するとTweetモデルのカウントが1上がることを確認する
+      # 投稿完了ページに遷移することを確認する
+      # 「投稿が完了しました」の文字があることを確認する
+      # トップページに遷移する
+      # トップページには先ほど投稿した内容のツイートが存在することを確認する（画像）
+      # トップページには先ほど投稿した内容のツイートが存在することを確認する（テキスト）
+    end
+  end
+  context 'ツイート投稿ができないとき'do
+    it 'ログインしていないと新規投稿ページに遷移できない' do
+      # トップページに遷移する
+      # 新規投稿ページへのボタンがないことを確認する
+    end
+  end
+  
 end
